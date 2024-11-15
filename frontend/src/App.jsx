a46382bd7b4970a06270827ce9acbe02a5c201c6
Expo-67 <145459052+Expo-67@users.noreply.github.com>
2024-11-15 21:03:11 +0300

    Frontend progression

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+//import FloatingShape from "./components/FloatingShape";
+import { LoginPage } from "./Pages/LoginPage";
+import { SignUpPage } from "./Pages/SignUpPage.jsx";
 function App() {
   return (
-    <div
-      className=" min-h-screen bg-gradient-to-br
-   
-   from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative
-    overflow-hidden"
-    >
+      {/* className="min-h-screen bg-gradient-to-br
+  from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden"
       <FloatingShape
         color="bg-green-500"
         size="w-64 h-64"
@@ -15,7 +15,7 @@ function App() {
       />
       <FloatingShape
         color="bg-emerald-500"
-        size="w-64 h-48"
+        size="w-48 h-48"
         top="70%"
         left="80%"
         delay={5}
@@ -26,8 +26,18 @@ function App() {
         top="40%"
         left="-10%"
         delay={2}
-      />
-    </div>
+      /> */}
+      <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/Profile" element={<ProfilePage />} />
+          <Route path="/WorkoutForm" element={<WorkoutFormPage />} />
+          <Route path="/WorkoutList" element={<WorkoutListPage />} />
+          <Route path="/ProgressGraph" element={<ProgressGraphPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/Pages/SignUpPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUpPage = () => {
+  return <div>This is the sign up page guys</div>;
+};
+
+export { SignUpPage };
